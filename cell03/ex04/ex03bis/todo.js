38e1159a541e3da58e986990dc203eae54362706
vsritawe <vsritawe@student.42bangkok.com>
2024-09-14 20:24:53 +0700

    Update todo.js

--- cell03/ex04/ex03bis/todo.js
+++ cell03/ex04/ex03bis/todo.js
@@ -1,52 +1,73 @@
 $(document).ready(function() {
-    function getCookies() {
-        const cookies = document.cookie.split('; ');
-        let cookieObj = {};
-        cookies.forEach(cookie => {
-            let [key, value] = cookie.split('=');
-            cookieObj[key] = decodeURIComponent(value);
-        });
-        return cookieObj;
+    //load todo from cookies
+    loadToDoList();
+
+    //load and display todo from cookies
+    function loadToDoList() {
+        const todos = getCookie('todoList');
+        if (todos) {
+            const todoArray = JSON.parse(todos);
+            todoArray.forEach(todo => addTodoToDOM(todo));
+        }
     }
 
-    function setCookie(name, value, days) {
-        const date = new Date();
-        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-        const expires = "expires=" + date.toUTCString();
-        document.cookie = name + "=" + encodeURIComponent(value) + ";" + expires + ";path=/";
+    // save todo into cookies
+    function saveToDoList() {
+        const todoItems = $('.todo-item');
+        const todoArray = todoItems.map(function() {
+            return $(this).text();
+        }).get();
+
+        setCookie('todoList', JSON.stringify(todoArray));
     }
 
-    function saveTodos() {
-        const todos = [];
-        $('.todo-item').each(function() {
-            todos.push($(this).text());
-        });
-        localStorage.setItem('todos', JSON.stringify(todos));
-    }
-
-    function loadTodos() {
-        const todos = JSON.parse(localStorage.getItem('todos')) || [];
-        todos.forEach(todo => addTodoToDOM(todo));
-    }
-
+    //add new todo to DOM
     function addTodoToDOM(todoText) {
-        const todoDiv = $('<div></div>').addClass('todo-item').text(todoText);
-        todoDiv.on('click', function() {
-            if (confirm('Do you want to remove this TO DO?')) {
+        const $ftList = $('#ft_list');
+        const $todoDiv = $('<div></div>').addClass('todo-item').text(todoText);
+
+        // remove todo on confirmation
+        $todoDiv.click(function() {
+            const confirmation = confirm('Do you want to remove this task?');
+            if (confirmation) {
                 $(this).remove();
-                saveTodos();
+                saveToDoList();
             }
         });
-        $('#ft_list').prepend(todoDiv);
+
+        // add new todo at the top
+        $ftList.prepend($todoDiv);
     }
 
-    $('#new').on('click', function() {
-        const todoText = prompt('Enter a new TO DO:');
-        if (todoText) {
-            addTodoToDOM(todoText);
-            saveTodos();
+    // create new todo
+    $('#New').click(function() {
+        const newTodo = prompt('Enter a new TO DO:');
+        if (newTodo && newTodo.trim()) {
+            addTodoToDOM(newTodo.trim());
+            saveToDoList();
         }
     });
 
-    loadTodos();
+    //get a cookie by name
+    function getCookie(name) {
+        const cookieArr = document.cookie.split(';');
+        for (let i = 0; i < cookieArr.length; i++) {
+            const cookiePair = cookieArr[i].split('=');
+            if (name == cookiePair[0].trim()) {
+                return decodeURIComponent(cookiePair[1]);
+            }
+        }
+        return null;
+    }
+
+    //set a cookie with a name, value, and day
+    function setCookie(name, value, days) {
+        let expires = '';
+        if (days) {
+            const date = new Date();
+            date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+            expires = `expires=${date.toUTCString()};`;
+        }
+        document.cookie = `${name}=${encodeURIComponent(value)}; ${expires}path=/`;
+    }
 });
